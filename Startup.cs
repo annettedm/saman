4013e86f054da04292e5339c476a744e63b2d012
unknown <annette.dmitruk@gmail.com>
2019-07-03 15:26:07 +0300

    Initial

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Saman.Startup))]
+namespace Saman
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
